d45f18abba2e88778461a23c15314b13b8cde9d7
Stephen <stephen.anthony.rivas@gmail.com>
2022-12-15 12:44:20 -0500

    added skeleton layout for charts

--- src/web-mui/src/App.js
+++ src/web-mui/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import Header from './Header.js';
 console.log(Header);
 function App() {
@@ -13,12 +14,24 @@ function App() {
   return (
     <div className="App">
       <header className="Header"><h1>DockerVision</h1></header>
-      <main className="Main">Main</main>
+      <Grid className="Main"container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid xs={6}>
+          <div style={{"font-size": "116px"}}>1🐳📊</div>
+        </Grid>
+        <Grid xs={6}>
+          <div style={{"font-size": "116px"}}>2🐳📊</div>
+        </Grid>
+        <Grid xs={6}>
+          <div style={{"font-size": "116px"}}>3🐳📊</div>
+        </Grid>
+        <Grid xs={6}>
+          <div style={{"font-size": "116px"}}>4🐳📊</div>
+        </Grid>
+      </Grid>
       <div className="Sidebar">
         <Stack spacing={2}>
           {buttons}
         </Stack>
-      
       </div>
     </div>
   );
