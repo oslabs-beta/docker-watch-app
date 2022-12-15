d49a9dbf814010018534a128213eea470335f261
Dan Pietsch <drpietsch14@gmail.com>
2022-12-15 16:48:24 -0500

    api queries database and pulls all data for a set period of time, not functional in docker

--- src/api/index-api.js
+++ src/api/index-api.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
-const {InfluxDB, Point} = require('@influxdata/influxdb-client');
-require('dotenv').config({path: '../../.env'})
+
+const controller = require('./controller');
+
+// console.log(controller.getStatsFromDB);
 
 const PORT = 8081;
 const HOST = '0.0.0.0';
@@ -9,55 +12,29 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(cors());
 
-const DB_URL = 'http://localhost:8086/';
-const DB_API_TOKEN = process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN;
-const DB_ORG = process.env.DB_INFLUXDB_INIT_ORG;
-const DB_BUCKET = process.env.DB_INFLUXDB_INIT_BUCKET;
+app.get('/', controller.getStatsFromDB, (req, res) => {
+  res.status(200).json(res.locals.data);
+});
 
-// console.log(DB_API_TOKEN);
+app.use('*', (req, res, next) => {
+  const errorObj = {
+    log: 'Page not found',
+    status: 404,
+    message: { err: 'Error 404: Page not Found' },
+  };
+  next(errorObj);
+});
 
-// connect to the influx db using url and token
-const influxDB = new InfluxDB({url: DB_URL, token: DB_API_TOKEN})
+app.use((err, req, res) => {
+  const defaultErr = {
+    log: `Express error handler caught unknown middleware error: ${err}`,
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  console.log(defaultErr.log);
+  res.status(defaultErr.status).send(defaultErr.message);
+});
 
-// specify the org to send query to
-const queryApi = influxDB.getQueryApi(DB_ORG);
-
-// write the query
-const query = `from(bucket: "${DB_BUCKET}") |> range(start: -1h)`
-queryApi.queryRows(query, {
-  next(row, tableMeta) {
-    const o = tableMeta.toObject(row)
-    console.log(`${o._time} ${o._measurement}: ${o._field}=${o._value}`)
-  },
-  error(error) {
-    console.error(error)
-    console.log('Finished ERROR')
-  },
-  complete() {
-    console.log('Finished SUCCESS')
-  },
-})
-
-
-// write the query
-
-
-
-// const writeApi = influxDB.getWriteApi(DB_ORG, DB_BUCKET)
-// writeApi.useDefaultTags({host: 'host1'})
-
-// const point = new Point('mem').floatField('used_percent', 23.43234543)
-// writeApi.writePoint(point)
-
-// writeApi
-//     .close()
-//     .then(() => {
-//         console.log('FINISHED')
-//     })
-//     .catch(e => {
-//         console.error(e)
-//         console.log('Finished ERROR')
-//     })
-
-
-
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
