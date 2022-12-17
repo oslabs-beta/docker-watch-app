b91b74d0fd4db31568066735925036e430c74deb
Dan Pietsch <drpietsch14@gmail.com>
2022-12-17 10:56:02 -0500

    Finished api connection to db and dockerization of api application. Deleted api-temp

--- src/api/controller.js
+++ src/api/controller.js
@@ -2,15 +2,18 @@ const { InfluxDB } = require('@influxdata/influxdb-client');
 require('dotenv').config({ path: '../../.env' });
 
 const DB_URL = process.env.DB_URL || 'http://127.0.0.1:8086';
-const DB_API_TOKEN = process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN;
-const DB_ORG = process.env.DB_INFLUXDB_INIT_ORG;
-const DB_BUCKET = process.env.DB_INFLUXDB_INIT_BUCKET;
+// const DB_URL = 'http://DB:8086';
+const DB_API_TOKEN = process.env.DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
+  || process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN;
+const DB_ORG = process.env.DOCKER_INFLUXDB_INIT_ORG
+  || process.env.DB_INFLUXDB_INIT_ORG;
+const DB_BUCKET = process.env.DOCKER_INFLUXDB_INIT_BUCKET
+  || process.env.DB_INFLUXDB_INIT_BUCKET;
 
 const controller = {};
 // eslint-disable-next-line no-unused-expressions
 controller.getStatsFromDB = (req, res, next) => {
-  console.log('hiiiiiiii');
-  console.log(DB_URL);
+  // console.log(DB_ORG);
 
   // connect to the influx db using url and token
   const influxDB = new InfluxDB({ url: DB_URL, token: DB_API_TOKEN });
@@ -31,7 +34,7 @@ controller.getStatsFromDB = (req, res, next) => {
       const o = tableMeta.toObject(row);
       // eslint-disable-next-line no-underscore-dangle
       data.push(`${o._time} ${o._measurement}: ${o._field}=${o._value}`);
-      console.log(data);
+      // console.log(data);
     },
     error(error) {
       console.log('Finished ERROR');
