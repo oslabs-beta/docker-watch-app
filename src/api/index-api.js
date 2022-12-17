fdc532379d01f2ccebeae6325df10af86fa4d5c4
Dan Pietsch <drpietsch14@gmail.com>
2022-12-17 12:41:40 -0500

    created mock data to serve to frontend

--- src/api/index-api.js
+++ src/api/index-api.js
@@ -12,12 +12,23 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-});
-
+// serve all data from db
 app.get('/api', controller.getStatsFromDB, (req, res) => {
   res.status(200).json(res.locals.data);
+});
+
+// TODO: serve mock data to frontend
+// send an object with all stats on a specific container id
+app.get('/api/container/:id', controller.getContainerStats, (req, res) => {
+  res.status(200).json(res.locals.stats);
+});
+// send an object with all running container ids and names
+app.get('/api/containers', controller.getContainers, (req, res) => {
+  res.status(200).json(res.locals.containers);
+});
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
 });
 
 app.use('*', (req, res, next) => {
