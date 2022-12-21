ae9a09d0a10fb7a0cf6cfd97b95bea3511b05474
BrynnSakell <brynnsakell@gmail.com>
2022-12-21 12:55:10 -0500

    added interval

--- src/web/client/Container.jsx
+++ src/web/client/Container.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 export default function Container({ id, text, setContainerData }) {
+  const [containerCalled, setContainerCalled] = useState(false)
   // fetch container data from server
   const containerOnClick = (id) => {
     fetch(`http://localhost:8081/api/v1/containers/${id}/stats`)
       .then((response) => response.json())
       .then((data) => {
+        console.log(id)
         console.log('data: ', data)
+        console.log(intervalRef, intervalRef.current)
         setContainerData(data);
+        // setContaienrCalled(true)
       })
+      // .then(callContainers(id))
       .catch((err) => console.log(err));
   };
+  const intervalRef = useRef(0);
+
+  const callContainers = (id) => {
+    const interval = setInterval(() => containerOnClick(id), 5000);
+    // console.log('hello')
+    // const statInterval = setInterval(() => containerOnClick(id), 5000);
+    // const clearInterval = clearInterval(statInterval)
+    // if (containerCalled === false) {
+    //   setContainerCalled(true)
+    //   containerOnClick(id);
+    //   statInterval();
+    //   // setInterval(() => containerOnClick(id), 5000);
+    // } {
+    //   clearInterval();
+    //   containerOnClick(id);
+    //   statInterval();
+    // }
+    // if (intervalRef.current) clearInterval(intervalRef.current);
+    // containerOnClick(id);
+    // intervalRef.current = setInterval(() => containerOnClick(id), 5000);
+  }
   return (
     <>
       <button
-        onClick={() => containerOnClick(id)}
+        onClick={() => callContainers(id)}
         className="btn btn-outline btn-accent min-w-full"
       >
         {text}
