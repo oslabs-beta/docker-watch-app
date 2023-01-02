793e4ae8f18987381668bf2816fa8d10bc8551a3
Brynn Sakell <brynnsakell@gmail.com>
2023-01-02 15:50:30 -0500

    added chart zoom and pan functionality

--- src/web/client/LineChart.js
+++ src/web/client/LineChart.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Chart } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+// import { Chart } from "react-chartjs-2";
+// import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "chart.js/auto"; //replaced line 2 with this
 import { Line } from "react-chartjs-2";
+// import * as zoom from "chartjs-plugin-zoom"
+// import "chartjs-plugin-zoom"
+import zoomPlugin from 'chartjs-plugin-zoom'
+Chart.register(zoomPlugin);
 
 function LineChart({ graphData, title }) {
   const options = {
@@ -16,10 +21,23 @@ function LineChart({ graphData, title }) {
           size: 20
         },
         text: title
+      },
+      zoom: {
+        zoom: {
+          wheel: {
+            enabled: true,
+            speed: 0.04
+          },
+          mode: 'x',
+        },
+        pan: {
+          enabled: true,
+          mode: 'x',
+        }
       }
     }
   }
-  return <Line className="w-20" data={graphData} options={options} />;
+  return <Line className="w-20" data={graphData} options={options} />
 }
 
 export default LineChart;
