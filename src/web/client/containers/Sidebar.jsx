de9498d1e3d69dcdb6ffbfbd6a021578b34865a1
Brynn Sakell <brynnsakell@gmail.com>
2023-01-09 14:22:31 -0500

    Updated time range button to improve UX.
    Co-authored-by: Nadia Abowitz <abowitzn@users.noreply.github.com>

--- src/web/client/containers/Sidebar.jsx
+++ src/web/client/containers/Sidebar.jsx
@@ -5,6 +5,7 @@ export default function Sidebar({ containerData, setContainerData }) {
   const [containerList, updateContainerList] = useState([]);
   // const timeFrame = useRef('1h');
   const [timeFrame, setTimeFrame] = useState('1h');
+  const [timeDisplay, setTimeDisplay] = useState('time range');
   /* contains the current running setInterval calling the
   function that requests the api for metric data for the last clicked container. */
   const intervalRef = useRef(0);
@@ -29,6 +30,23 @@ export default function Sidebar({ containerData, setContainerData }) {
     getContainers();
   }, []);
 
+  const timeRangeClicked = (str) => {
+    setTimeFrame(str);
+
+    const times = {
+      '1m': 'past 1 minute',
+      '5m': 'past 5 minutes',
+      '1h': 'past 3 hours',
+      '3h': 'past 4 hours',
+      '6h': 'past 6 hours',
+      '12h': 'past 12 hours',
+      '1d': 'past 1 day',
+      '7d': 'past 7 days',
+    };
+
+    setTimeDisplay(times[str]);
+  };
+
   // iterates through contaierList and returns an array of container buttons to render to sidebar
   const containerButtons = containerList.map((container) => (
     <Container
@@ -46,17 +64,23 @@ export default function Sidebar({ containerData, setContainerData }) {
   return (
     <section className='Sidebar bg-slate-900'>
       <div className='pt-2'>
-        <div className='dropdown dropdown-right min-w-full block'>
-          <label tabIndex={0} className='btn m-1 min-w-full'>Click to change time frame</label>
+        <div className='dropdown dropdown-hover min-w-full block'>
+          <label tabIndex={0} className='btn mb-1 min-w-full border-white'>
+            🕐 &nbsp;
+            {' '}
+            {timeDisplay}
+            {' '}
+&nbsp;&nbsp;&nbsp; ↓
+          </label>
           <ul tabIndex={0} className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52'>
-            <li onClick={(e) => setTimeFrame('1m')}><a>1 minute</a></li>
-            <li onClick={(e) => setTimeFrame('5m')}><a>5 minutes</a></li>
-            <li onClick={(e) => setTimeFrame('1h')}><a>1 hour</a></li>
-            <li onClick={(e) => setTimeFrame('3h')}><a>3 hours</a></li>
-            <li onClick={(e) => setTimeFrame('6h')}><a>6 hours</a></li>
-            <li onClick={(e) => setTimeFrame('12h')}><a>12 hours</a></li>
-            <li onClick={(e) => setTimeFrame('1d')}><a>1 day</a></li>
-            <li onClick={(e) => setTimeFrame('7d')}><a>7 days</a></li>
+            <li onClick={() => timeRangeClicked('1m')}><a>Past 1 minute</a></li>
+            <li onClick={() => timeRangeClicked('5m')}><a>Past 5 minutes</a></li>
+            <li onClick={() => timeRangeClicked('1h')}><a>Past 1 hour</a></li>
+            <li onClick={() => timeRangeClicked('3h')}><a>Past 3 hours</a></li>
+            <li onClick={() => timeRangeClicked('6h')}><a>Past 6 hours</a></li>
+            <li onClick={() => timeRangeClicked('12h')}><a>Past 12 hours</a></li>
+            <li onClick={() => timeRangeClicked('1d')}><a>Past 1 day</a></li>
+            <li onClick={() => timeRangeClicked('7d')}><a>Past 7 days</a></li>
           </ul>
         </div>
         {containerButtons}
