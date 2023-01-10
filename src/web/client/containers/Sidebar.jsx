3798e3a1a21b979d548070aa93e468c01b90c118
Brynn Sakell <brynnsakell@gmail.com>
2023-01-10 15:40:20 -0500

    fixed time range text bug
    Co-authored-by: Nadia Abowitz <abowitzn@users.noreply.github.com>

--- src/web/client/containers/Sidebar.jsx
+++ src/web/client/containers/Sidebar.jsx
@@ -34,14 +34,14 @@ export default function Sidebar({ containerData, setContainerData }) {
     setTimeFrame(str);
 
     const times = {
-      '1m': 'past 1 minute',
-      '5m': 'past 5 minutes',
-      '1h': 'past 3 hours',
-      '3h': 'past 4 hours',
-      '6h': 'past 6 hours',
-      '12h': 'past 12 hours',
-      '1d': 'past 1 day',
-      '7d': 'past 7 days',
+      '1m': 'Past 1 minute',
+      '5m': 'Past 5 minutes',
+      '1h': 'Past 1 hour',
+      '3h': 'Past 3 hours',
+      '6h': 'Past 6 hours',
+      '12h': 'Past 12 hours',
+      '1d': 'Past 1 day',
+      '7d': 'Past 7 days',
     };
 
     setTimeDisplay(times[str]);
